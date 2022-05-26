2b978de378e43a223ecb906c4eb64191bed46bf5
Vinicius Marques Gallichio <83302966+ViniciusGallichio@users.noreply.github.com>
2022-05-26 16:55:50 -0300

    Add files via upload
    Commit feito as 16:55, 26/05

--- HomePage.java
+++ HomePage.java
@@ -1,3 +1,10 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
@@ -26,31 +33,290 @@ public class HomePage extends javax.swing.JFrame {
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
-
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        jButton1 = new javax.swing.JButton();
+        jTabbedPane1 = new javax.swing.JTabbedPane();
+        jPanel2 = new javax.swing.JPanel();
+        mesa01 = new javax.swing.JButton();
+        mesa02 = new javax.swing.JButton();
+        mesa03 = new javax.swing.JButton();
+        mesa04 = new javax.swing.JButton();
+        mesa05 = new javax.swing.JButton();
+        mesa06 = new javax.swing.JButton();
+        jPanel1 = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        caixaTxtRegistroUsuario = new javax.swing.JTextField();
+        caixaTxtRegistroCargo = new javax.swing.JTextField();
+        caixaTxtRegistroSenha = new javax.swing.JTextField();
+        btRegistrar = new javax.swing.JButton();
 
         jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 48)); // NOI18N
         jLabel1.setText("Home page");
+
+        jButton1.setText("jButton1");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jTabbedPane1.setForeground(new java.awt.Color(51, 51, 51));
+        jTabbedPane1.setFont(new java.awt.Font("Segoe UI", 2, 14)); // NOI18N
+
+        mesa01.setFont(new java.awt.Font("Segoe UI", 3, 14)); // NOI18N
+        mesa01.setText("Mesa01");
+        mesa01.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mesa01ActionPerformed(evt);
+            }
+        });
+
+        mesa02.setFont(new java.awt.Font("Segoe UI", 3, 14)); // NOI18N
+        mesa02.setText("Mesa02");
+        mesa02.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mesa02ActionPerformed(evt);
+            }
+        });
+
+        mesa03.setFont(new java.awt.Font("Segoe UI", 3, 14)); // NOI18N
+        mesa03.setText("Mesa03");
+        mesa03.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mesa03ActionPerformed(evt);
+            }
+        });
+
+        mesa04.setFont(new java.awt.Font("Segoe UI", 3, 14)); // NOI18N
+        mesa04.setText("Mesa04");
+        mesa04.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mesa04ActionPerformed(evt);
+            }
+        });
+
+        mesa05.setFont(new java.awt.Font("Segoe UI", 3, 14)); // NOI18N
+        mesa05.setText("Mesa05");
+        mesa05.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mesa05ActionPerformed(evt);
+            }
+        });
+
+        mesa06.setFont(new java.awt.Font("Segoe UI", 3, 14)); // NOI18N
+        mesa06.setText("Mesa06");
+        mesa06.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mesa06ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addContainerGap(47, Short.MAX_VALUE)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(mesa01, javax.swing.GroupLayout.PREFERRED_SIZE, 86, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(mesa04, javax.swing.GroupLayout.PREFERRED_SIZE, 86, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(90, 90, 90)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(mesa05, javax.swing.GroupLayout.PREFERRED_SIZE, 86, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(mesa02, javax.swing.GroupLayout.PREFERRED_SIZE, 86, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(79, 79, 79)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(mesa03, javax.swing.GroupLayout.PREFERRED_SIZE, 86, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(mesa06, javax.swing.GroupLayout.PREFERRED_SIZE, 86, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(41, 41, 41))
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(73, 73, 73)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(mesa01, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(mesa02, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(mesa03, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(55, 55, 55)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(mesa04, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(mesa06, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(mesa05, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(87, 87, 87))
+        );
+
+        jTabbedPane1.addTab("Mesas", jPanel2);
+
+        jLabel2.setFont(new java.awt.Font("Segoe UI", 1, 13)); // NOI18N
+        jLabel2.setText("Usuario");
+
+        jLabel3.setFont(new java.awt.Font("Segoe UI", 1, 13)); // NOI18N
+        jLabel3.setText("Senha");
+
+        jLabel4.setFont(new java.awt.Font("Segoe UI", 1, 13)); // NOI18N
+        jLabel4.setText("Cargo");
+
+        caixaTxtRegistroUsuario.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                caixaTxtRegistroUsuarioActionPerformed(evt);
+            }
+        });
+
+        caixaTxtRegistroCargo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                caixaTxtRegistroCargoActionPerformed(evt);
+            }
+        });
+
+        caixaTxtRegistroSenha.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                caixaTxtRegistroSenhaActionPerformed(evt);
+            }
+        });
+
+        btRegistrar.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        btRegistrar.setText("Registrar");
+        btRegistrar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btRegistrarActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addGap(27, 27, 27)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addGap(18, 18, 18)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(caixaTxtRegistroUsuario, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(caixaTxtRegistroSenha, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 51, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(btRegistrar, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(caixaTxtRegistroCargo, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addGap(624, 624, 624))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(28, 28, 28)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(caixaTxtRegistroUsuario, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(27, 27, 27)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(caixaTxtRegistroSenha, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(31, 31, 31)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(caixaTxtRegistroCargo, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addComponent(btRegistrar, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        jTabbedPane1.addTab("Registrar", jPanel1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(171, 171, 171)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 290, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(149, Short.MAX_VALUE))
+            .addComponent(jTabbedPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 515, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(171, 171, 171)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 86, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(183, Short.MAX_VALUE))
+            .addComponent(jTabbedPane1)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // Esse botão não existe "BOTÃO FANTASMA BRO WTF"
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void mesa01ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mesa01ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_mesa01ActionPerformed
+
+    private void mesa02ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mesa02ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_mesa02ActionPerformed
+
+    private void mesa04ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mesa04ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_mesa04ActionPerformed
+
+    private void caixaTxtRegistroUsuarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_caixaTxtRegistroUsuarioActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_caixaTxtRegistroUsuarioActionPerformed
+
+    private void caixaTxtRegistroSenhaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_caixaTxtRegistroSenhaActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_caixaTxtRegistroSenhaActionPerformed
+
+    private void mesa03ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mesa03ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_mesa03ActionPerformed
+
+    private void mesa05ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mesa05ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_mesa05ActionPerformed
+
+    private void mesa06ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mesa06ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_mesa06ActionPerformed
+
+    private void caixaTxtRegistroCargoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_caixaTxtRegistroCargoActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_caixaTxtRegistroCargoActionPerformed
+
+    private void btRegistrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btRegistrarActionPerformed
+        // TODO add your handling code here:
+         try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/restaurante","root","12345678vini@");
+           
+            String usuario = caixaTxtRegistroUsuario.getText();
+            String senha = caixaTxtRegistroSenha.getText();
+            String cargo = caixaTxtRegistroCargo.getText();
+            
+            Statement stm = con.createStatement();
+            
+            if(usuario.length() <= 1 && senha.length() <= 1 && cargo.length() <= 1){
+                JOptionPane.showMessageDialog(this, "Insira o usuario, senha e cargo corretamente!");
+                con.close();
+            }
+
+            stm.executeUpdate("insert into login (usuario_login, senha_login, cargo_login)" + "values ('"+usuario+"', '"+senha+"', '"+cargo+"')");
+            
+            
+            JOptionPane.showMessageDialog(this, "Registro criado com sucesso!");
+            caixaTxtRegistroUsuario.setText("");
+            caixaTxtRegistroSenha.setText("");
+            caixaTxtRegistroCargo.setText("");
+           
+            con.close();
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }//GEN-LAST:event_btRegistrarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -88,6 +354,23 @@ public class HomePage extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btRegistrar;
+    private javax.swing.JTextField caixaTxtRegistroCargo;
+    private javax.swing.JTextField caixaTxtRegistroSenha;
+    private javax.swing.JTextField caixaTxtRegistroUsuario;
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JTabbedPane jTabbedPane1;
+    private javax.swing.JButton mesa01;
+    private javax.swing.JButton mesa02;
+    private javax.swing.JButton mesa03;
+    private javax.swing.JButton mesa04;
+    private javax.swing.JButton mesa05;
+    private javax.swing.JButton mesa06;
     // End of variables declaration//GEN-END:variables
 }
--- Restaurante.java
+++ Restaurante.java
@@ -13,7 +13,7 @@ public class Restaurante {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
         Login logar = new Login();
         logar.show();
     }
